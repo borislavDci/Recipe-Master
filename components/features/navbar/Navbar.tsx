eb13c6a6e764caf1e23ce5ea86a00670379a7159
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-02-27 00:24:25 +0100

    bug fixed

--- components/features/navbar/Navbar.tsx
+++ components/features/navbar/Navbar.tsx
@@ -78,7 +78,7 @@ const Navbar = () => {
       </div>
 
       <div className={`w-full ${isMenuOpen ? "block" : "hidden"}`}>
-        <ul className="flex flex-col items-center rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+        <ul className="rtl:space-x-revers flex flex-col  items-center border-0 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 dark:bg-gray-700">
           {/* Renders NavItems components to dispay the nav items */}
           <NavItems items={navItems} />
           <li>
