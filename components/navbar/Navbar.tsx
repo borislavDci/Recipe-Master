f5f30fe3dd425003d244047678ce8e18d59bc016
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-02-26 15:53:20 +0100

    bug fixed

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
-import profile from "@/public/profile.png";
+import { HiMenu, HiX } from "react-icons/hi"; // Import icons for menu
+
+import profile from "@/public/navbar/profile.png";
+import logo from "@/public/navbar/logo.png";
 import ProfileDropdown from "./ProfileDropdown";
 
 const Navbar = () => {
@@ -21,14 +25,14 @@ const Navbar = () => {
   return (
     <nav className="relative border-gray-200 bg-white dark:bg-gray-900">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <a
-          href="https://flowbite.com/"
+        <Link
+          href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-            Flowbite
+            Recipe Master
           </span>
-        </a>
+        </Link>
         <div className="relative flex items-center space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
@@ -54,26 +58,18 @@ const Navbar = () => {
             aria-expanded={isMenuOpen ? "true" : "false"}
             onClick={toggleMenu}
           >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="h-5 w-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
+            <span className="sr-only">Toggle menu</span>
+            {isMenuOpen ? (
+              <HiX className="h-5 w-5" />
+            ) : (
+              <HiMenu className="h-5 w-5" />
+            )}
           </button>
         </div>
         <div
-          className={`w-full items-center justify-between md:order-1 md:flex md:w-auto ${isMenuOpen ? "" : "hidden"}`}
+          className={`w-full items-center justify-between md:order-1 md:flex md:w-auto ${
+            isMenuOpen ? "" : "hidden"
+          }`}
           id="navbar-user"
         >
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
