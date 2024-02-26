ccec82acd9852c83f3573be540873b4a402c2100
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-02-26 16:39:07 +0100

    navbar has been optomized

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,11 +2,14 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { HiMenu, HiX } from "react-icons/hi"; // Import icons for menu
-
+import { HiMenu, HiX } from "react-icons/hi";
 import profile from "@/public/navbar/profile.png";
-import logo from "@/public/navbar/logo.png";
 import ProfileDropdown from "./ProfileDropdown";
+
+interface NavItem {
+  title: string;
+  href: string;
+}
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -21,6 +24,12 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
     setIsDropdownOpen(false); // Close the dropdown when opening menu
   };
+
+  // Defines an array of navigation items
+  const navItems: NavItem[] = [
+    { title: "Home", href: "/" },
+    { title: "About", href: "/about" },
+  ];
 
   return (
     <nav className="relative border-gray-200 bg-white dark:bg-gray-900">
@@ -60,9 +69,9 @@ const Navbar = () => {
           >
             <span className="sr-only">Toggle menu</span>
             {isMenuOpen ? (
-              <HiX className="h-5 w-5" />
+              <HiX className="h-6 w-6" />
             ) : (
-              <HiMenu className="h-5 w-5" />
+              <HiMenu className="h-6 w-6" />
             )}
           </button>
         </div>
@@ -73,23 +82,17 @@ const Navbar = () => {
           id="navbar-user"
         >
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
-            <li>
-              <a
-                href="#"
-                className="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              >
-                About
-              </a>
-            </li>
+            {/* Renders Navbar Items */}
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- components/navbar/ProfileDropdown.tsx
+++ components/navbar/ProfileDropdown.tsx
@@ -1,15 +1,17 @@
 "use client";
 import Link from "next/link";
+import { MdDashboardCustomize } from "react-icons/md";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
 const ProfileDropdown = ({ isDropdownOpen }: { isDropdownOpen: boolean }) => {
   return (
     <div
-      className={`absolute left-3 top-full z-50 -translate-x-full transform ${
+      className={`absolute left-6 top-full z-50 w-48 -translate-x-full transform ${
         isDropdownOpen ? "" : "hidden"
       } my-4 list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:divide-gray-600 dark:bg-gray-700`}
       id="user-dropdown"
     >
-      <div className="px-4 py-3">
+      <div className="px-4 py-4">
         <span className="block text-sm text-gray-900 dark:text-white">
           John Doe
         </span>
@@ -23,6 +25,7 @@ const ProfileDropdown = ({ isDropdownOpen }: { isDropdownOpen: boolean }) => {
             href="#"
             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
           >
+            <MdDashboardCustomize className="dashboardIcons" />
             Dashboard
           </Link>
         </li>
@@ -32,6 +35,7 @@ const ProfileDropdown = ({ isDropdownOpen }: { isDropdownOpen: boolean }) => {
             href="#"
             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
           >
+            <RiLogoutBoxRLine className="dashboardIcons" />
             Logout
           </Link>
         </li>
